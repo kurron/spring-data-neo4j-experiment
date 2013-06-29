d2be5c91964f739a57d507d8dcf5433ec79830be
Ron Kurr <kurr@kurron.org>
2013-06-28 21:47:25 -0400

    Started working on the relationship tutorial.

--- src/test/groovy/org/kurron/spring/data/graph/example/one/MovieRepository.groovy
+++ src/test/groovy/org/kurron/spring/data/graph/example/one/MovieRepository.groovy
@@ -1,5 +1,6 @@
 package org.kurron.spring.data.graph.example.one
 
+import org.springframework.data.neo4j.annotation.Query
 import org.springframework.data.neo4j.repository.GraphRepository
 
 /**
@@ -9,4 +10,7 @@ public interface MovieRepository extends GraphRepository<Movie> {
 
     // a custom query method
     Movie getMovieById( String id )
+
+    @Query( 'start user=node:User({0}) match user-[r:RATED]->movie return movie order by r.stars desc limit 10' )
+    Iterable<Movie> getTopRatedMovies( User user )
 }
